92cb7989fe98bf743b6712dfd1de70e33b5174f4
veecodes14 <134261808+veecodes14@users.noreply.github.com>
2025-04-04 19:39:12 +0000

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def home (request):
+    content = {'posts': Post.objects.all()}
+    return render(request, 'blog/home.html', content)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
